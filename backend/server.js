45d4893622a3e80961f3b8e8ab665ad03d08e509
Devin <nguyen.devin@outlook.com>
2023-08-16 21:43:05 -0700

    Add post route to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectToDatabase from "./database/database.js"
+import postRoute from "./routes/postRoute.js"
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 connectToDatabase()
+
+app.use('/posts', postRoute);
 
 app.listen(port, () => {
     console.log(`Server Listening on Port ${port}`)
