a6dad5c6243bfce235c809d57b908eb3818ef383
Devin <nguyen.devin@outlook.com>
2023-08-16 19:44:25 -0700

    Set up environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 // index.js
 
+require("dotenv").config();
+
 const express = require('express')
 const app = express()
-const port = 3001;
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
