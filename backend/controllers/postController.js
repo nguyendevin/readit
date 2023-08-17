93d1738fcf4ef03b97472b7a06af86ecb01cca9a
Devin <nguyen.devin@outlook.com>
2023-08-16 22:03:05 -0700

    Add try and catch blocks to prevent crash

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -6,35 +6,55 @@ import Post from "../models/postModel.js"
 const router = express.Router()
 
 export const getPosts = async (req, res) => {
-    const posts = await Post.find()
-    res.json(posts)
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (e) {
+        res.json({ message: e.message })
+    }
 }
 
 export const getPost = async (req, res) => {
-    const { id } = req.params
-    const post = await Post.findById(id)
-    res.json(post)
+    try {
+        const { id } = req.params
+        const post = await Post.findById(id)
+        res.json(post)
+    } catch (e) {
+        res.json({ message: e.message })
+    }
 }
 
 export const addPost = async (req, res) => {
-    const { content } = req.body
-    const newPost = new Post({ content })
-    await newPost.save()
-    res.json(newPost)
+    try {
+        const { content } = req.body
+        const newPost = new Post({ content })
+        await newPost.save()
+        res.json(newPost)
+    } catch (e) {
+        res.json({ message: e.message })
+    }
 }
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params
-    await Post.findByIdAndRemove(id)
-    res.json({ message: `Deleted Post ${id}` })
+    try {
+        const { id } = req.params
+        await Post.findByIdAndRemove(id)
+        res.json({ message: `Deleted Post ${id}` })
+    } catch (e) {
+        res.json({ message: e.message })
+    }
 }
 
 export const patchPost = async (req, res) => {
-    const { id } = req.params
-    const { content } = req.body
-    const patchedPost = { content, _id: id }
-    await Post.findByIdAndUpdate(id, patchedPost, { new: true })
-    res.json(patchedPost)
+    try {
+        const { id } = req.params
+        const { content } = req.body
+        const patchedPost = { content, _id: id }
+        await Post.findByIdAndUpdate(id, patchedPost, { new: true })
+        res.json(patchedPost)
+    } catch (e) {
+        res.json({ message: e.message })
+    }
 }
 
 export default router
