94b6e428e5eaabfad4c643635ca88d88dfc44cf3
Devin <nguyen.devin@outlook.com>
2023-08-16 21:04:25 -0700

    Create post schema

---
+++ backend/models/postModel.js
@@ -0,0 +1,11 @@
+// userModel.js
+
+import mongoose from "mongoose"
+
+const postSchema = mongoose.Schema({
+    content: String
+})
+
+const Post = mongoose.model("Post", postSchema)
+
+export default Post
