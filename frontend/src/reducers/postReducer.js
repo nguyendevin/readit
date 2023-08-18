7833fd48493dffb92b87d5d6a36d1205f3c8b8a1
Devin <nguyen.devin@outlook.com>
2023-08-18 15:52:59 -0700

    Configure post reducer

--- frontend/src/reducers/postReducer.js
+++ frontend/src/reducers/postReducer.js
@@ -1,1 +1,24 @@
 // postReducer.js
+
+import { combineReducers } from "redux"
+
+const postReducer = (state = [], action) => {
+    switch (action.type) {
+        case "GET_POSTS":
+            return action.payload
+        case "GET_POST":
+            return state.find((post) => post._id === action.payload._id)
+        case "ADD_POST":
+            return [...state, action.payload]
+        case "DELETE_POST":
+            return state.filter((post) => post._id !== action.payload._id)
+        case "PATCH_POST":
+            return state.map((post) => (post._id === action.payload._id ? action.payload : post))
+        case "ADD_COMMENT":
+            return state.map((post) => (post._id === action.payload._id ? action.payload : post))
+        default:
+            return state
+    }
+}
+
+const reducers = combineReducers({ postReducer })
