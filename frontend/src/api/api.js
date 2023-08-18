a4b5e775b551724f1b6d263c20a8b7c866c77ff3
Devin <nguyen.devin@outlook.com>
2023-08-18 15:30:41 -0700

    Configure api

---
+++ frontend/src/api/api.js
@@ -0,0 +1,31 @@
+// api.js
+
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://localhost:3001"
+})
+
+export const getPosts = () => {
+    api.get("/posts")
+}
+
+export const getPost = (id) => {
+    api.get(`/posts/${id}`)
+}
+
+export const addPost = (post) => {
+    api.post("/posts", post)
+}
+
+export const deletePost = (id) => {
+    api.delete(`/posts/${id}`)
+}
+
+export const patchPost = (id, post) => {
+    api.patch(`/posts/${id}`, post)
+}
+
+export const addComment = (id, comment) => {
+    api.post(`/posts/${id}/addComment`, { comment })
+}
