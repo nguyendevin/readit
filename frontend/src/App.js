a4666a189ab5572d88d3b97d233af4b172cf70bd
Devin <nguyen.devin@outlook.com>
2023-08-18 20:48:35 -0700

    Commiting before reverting Post changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 // App.js
 
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
 import Form from "./components/Form/Form"
-import Posts from "./components/Posts/Posts"
+import Post from "./components/Post/Post"
+import { getPosts } from "../../actions/postAction"
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
+  
   return (
     <div>
       <Form />
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,7 +1,12 @@
 // Post.jsx
 
-const Post = () => {
+import { useState } from "react"
+import { useDispatch } from "react-redux"
 
+import { deletePost, addComment } from "../../actions/postAction"
+
+const Post = () => {
+    
 }
 
 export default Post
