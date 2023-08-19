2ee6217d0adb8f29d97e2fe20cef7cdb67ed3374
Devin <nguyen.devin@outlook.com>
2023-08-18 19:38:41 -0700

    Set up index with Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,18 @@
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
 
 import App from "./App"
+import { reducers } from "./reducers/postReducer"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(thunk))
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
